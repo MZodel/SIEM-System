2dbfe7d8c0a508153b9e14eb49a1e1fd86f28dcb
Zodel <m.zodel@hotmail.de>
2018-11-20 18:06:19 +0100

    Setup-Test-Klassen geschrieben

--- SIEM/src/test/java/PersonEvent.java
+++ SIEM/src/test/java/PersonEvent.java
@@ -1,4 +1,18 @@
 
 public class PersonEvent {
-
+	private String name;
+	private int age;
+	
+	public PersonEvent(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
 }
